c4e48ef72e4a1659c585b77ed6eaeaad8d3abfbd
Tyler Bindon <martica@martica.org>
2013-01-19 15:48:34 -0700

    Added examples of different imports

--- src/template.rs
+++ src/template.rs
@@ -5,6 +5,14 @@ mod nested_module {
     mod submodule;
 }
 
+use io::println;
+use module::greeter;
+use nested_module::submodule::world;
+
+use expound = io::println;
+use doorman = module::greeter;
+use le_monde = nested_module::submodule::world;
+
 #[test]
 fn test_testing() {
     assert( 1 == 1 );
@@ -17,5 +25,12 @@ fn test_failing() {
 }
 
 fn main() {
+    // Explict paths
     io::println(module::greeter(nested_module::submodule::world()));
+
+    // Implict paths -- made possible by use statements
+    println(greeter(world()));
+
+    // Renamed -- made possible by use .. = statements
+    expound(doorman(le_monde()));
 }
